e41ad699eb93c1257c9fef79f45f6c1bb22d7c84
cardguy <rob@cardscreative.com>
2012-06-28 12:09:38 -0600

    Update master

--- lib/simple_twitter.rb
+++ lib/simple_twitter.rb
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
 require('httparty')
 require('json')
 
 require('simple_twitter/search')
 require('simple_twitter/version')
-=======
-require('simple_twitter/version')
-require('httparty')
-require('simple_twitter/search')
->>>>>>> 8a5750d102e516bd511d245121729a06d846ac52
 
 module SimpleTwitter
   # Your code goes here...
